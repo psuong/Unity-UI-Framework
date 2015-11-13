ef28802fe72a72d1c100f736b333491976e0dd3c
Porrith Suong <psuong95@gmail.com>
2015-11-12 23:00:26 -0500

    Initial commit.

---
+++ Assets/Core Module/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Utilities
+{
+
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        private T instance;
+        private static object _lock = new object();
+
+        public static T instance
+        {
+            get
+            {
+
+            }
+        }
+    }
+}
